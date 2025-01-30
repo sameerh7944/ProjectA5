d457c845f5b6b2655d144a9bc6d30c7854306d3e
sameerh7944 <sayedsameer250@gmail.com>
2025-01-29 21:23:33 -0500

    Update DemoTest5.java
    test code

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -11,7 +11,10 @@ public class DemoTest5 {
 		int result = a + b;
 		
 		System.out.println(result);
-		
+		int x = 10;
+		int y = 20;
+		if(x > y) {
+		System.out.println("Greater")
 	}
 		public void add(int a, int b) {
 			System.out.println(a + b);
