f8130824a311cffafa5208e9099b382617a30be4
sayed <sayed@Sameer.mynetworksettings.com>
2025-01-29 20:58:37 -0500

    implemented add()

--- src/Demo1/DemoTest5.java
+++ src/Demo1/DemoTest5.java
@@ -4,5 +4,17 @@ public class DemoTest5 {
 
 	public static void main(String[]args) {
 		System.out.println("Java");
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
+		
 	}
-}
+		public void add(int a, int b) {
+			System.out.println(a + b);
+		}
+	}
+
